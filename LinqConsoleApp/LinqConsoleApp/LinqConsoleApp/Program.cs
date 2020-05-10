c26be72eaceae720e02791eeb7dea24b179a97e0
oskar k <s19732@pjwstk.edu.pl>
2020-05-10 21:13:26 +0200

    almost done

---
+++ LinqConsoleApp/LinqConsoleApp/LinqConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LinqConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var d = new LinqSamples();
+
+
+           
+            d.Task12();
+        }
+    }
+}
